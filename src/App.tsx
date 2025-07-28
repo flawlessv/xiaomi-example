131f7be632fac22b6c15cd2de24f7bded341f32d
zhaoshiwei1 <zhaoshiwei1@xiaomi.com>
2025-07-28 14:44:10 +0800

    移除 TablePage 组件及其在 App 组件中的引用

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import './App.css';
-import TablePage from './components/TablePage';
-
 function App() {
   return (
     <div className="App">
-      <TablePage />
     </div>
   );
 }
